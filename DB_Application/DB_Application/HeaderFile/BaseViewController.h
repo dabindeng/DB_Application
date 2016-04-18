3ef33f2048b7ab9f2dffd13e2aef1ef87a9c3ced
dengyb <515829354@qq.com>
2016-04-18 17:28:36 +0800

    #0000  根控制器

---
+++ DB_Application/DB_Application/HeaderFile/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  DB_Application
+//
+//  Created by epwk on 16/4/18.
+//  Copyright © 2016年 DBAPP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
